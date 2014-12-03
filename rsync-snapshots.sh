0dec1aa68840154a5972e881264ca8a75e6c1f84
Artur Shaik <ashaihullin@gmail.com>
2014-12-03 22:51:15 +0600

    Syntax fixes

--- rsync-snapshots.sh
+++ rsync-snapshots.sh
@@ -10,32 +10,35 @@ CONFIGURATION_ERROR=5
 function show_help
 {
     cat << "EOF"
-    USAGE: rsync-snapshot.sh [CONFIGURATION_FILE]
+    USAGE: 
+        rsync-snapshot.sh [CONFIGURATION_FILE]
 
-    PURPOSE: create a snapshot backup of specified directories/files into remote server via rsync over ssh. 
+    PURPOSE: 
+        Create a snapshot backup of specified directories/files into remote 
+        server via rsync over ssh. 
 
-    If no CONFIGURATION_FILE, local one (default.rshot.conf) will be used.
+        If no CONFIGURATION_FILE, local one (default.rshot.conf) will be used.
 
-    Rsync over ssh is used to transfer the files with a delta-transfer 
-    algorithm to transfer only minimal parts of the files and improve 
-    speed; rsync uses for this the previous backup as reference.
-    This reference is also used to create hard links instead of files when
-    possible and thus save disk space. If original and reference file have
-    identical content but different timestamps or permissions then no hard link
-    is created.
-    A rotation of all backups renames snapshot.X into snapshot.X+1 and removes
-    backups with X>512. About 10 backups with non-linear distribution are kept
-    in rotation; for example with X=1,2,3,4,8,16,32,64,128,256,512.
-    The snapshots folders are protected read-only against all users including
-    root using 'chattr'.
+        Rsync over ssh is used to transfer the files with a delta-transfer 
+        algorithm to transfer only minimal parts of the files and improve 
+        speed; rsync uses for this the previous backup as reference.
+        This reference is also used to create hard links instead of files when
+        possible and thus save disk space. If original and reference file have
+        identical content but different timestamps or permissions then no hard link
+        is created.
+        A rotation of all backups renames snapshot.X into snapshot.X+1 and removes
+        backups with X>512. About 10 backups with non-linear distribution are kept
+        in rotation; for example with X=1,2,3,4,8,16,32,64,128,256,512.
+        The snapshots folders are protected read-only against all users including
+        root using 'chattr'.
 
     FILES:
-    rsync-snapshot.sh       the backup script, entry point
-    rsync-include.txt       rsync filter rules
-    default.rshot.conf      default example configuration
-    remove_old.sh, 
-    remove_snapshot.sh, 
-    rotate_and_clean_up.sh  inner helpers, don't use it directly
+        rsync-snapshot.sh       the backup script, entry point
+        rsync-include.txt       rsync filter rules
+        default.rshot.conf      default example configuration
+        remove_old.sh, 
+        remove_snapshot.sh, 
+        rotate_and_clean_up.sh  inner helpers, don't use it directly
 
 EOF
 
@@ -298,6 +301,6 @@ function main
     exit $SUCCESS
 }
 
-main
+main $@
 #eof
 
